3f8418892506b31f6f1b535ecabd7aa896d66005
shahab <shahab.ramzan@codedistrict.com>
2021-12-27 11:59:33 +0500

    jest configs added

---
+++ src/__mocks__/index.test.js
@@ -0,0 +1,6 @@
+// In this folder I place the mocks for installed modules
+// I will use in any integration or unit tests. Mocks must have the
+// same name of the imported file, in cases like next/link which are nested modules
+//  we need to create a next folder and put the link.js file inside it.
+
+// https://morioh.com/p/bc86959800b9
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,2 @@
+// Here are the integration tests, each integration tests should
+// match a page inside the pages directory.
